8e5a7c9ecdcf9a38af135ce9aa1645d119566485
Aliou Sidi Maiga <badrasadja@gmail.com>
2015-06-25 09:46:43 -0700

    few changes

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ http = require("http"),
 request = require('request'),
 url = require('url'),
 marked = require('marked');
-// angular.module("mark", [])
-// .controller("markController", function($scope) {
-//   $scope.marks = []});
+
 
 http.createServer(responseHandler).listen(process.env.PORT || 8888);
 function responseHandler(req, res) {
  res.writeHead(200, {"Content-Type": "text/html"});
  if (req.url === "/") {
+   fs.readFile('index.html', 'utf8', function(err, data){
+     res.end(data);
+   })
    res.end();
  } else if (req.url.match("/markdown/")) {
    var urlValue = "";
