e4ad56d1f77b9bbb125580b1b39c406e49b6a8d2
Aliou Sidi Maiga <badrasadja@gmail.com>
2015-06-25 14:04:00 -0700

    half working

--- main.js
+++ main.js
@@ -5,17 +5,15 @@ url = require('url'),
 marked = require('marked');
 http.createServer(responseHandler).listen(process.env.PORT || 8888);
 function responseHandler(req, res) {
- res.writeHead(200, {"Content-Type": "text/html"});
- if (req.url === "/") {
-   fs.readFile('index.html', 'utf8', function(err, data){
-     res.end(data);
-   })
-   res.end();
- } else if (req.url.match("/markdown/")) {
-   var urlValue = "";
-   urlValue = decodeURI(req.url.match(/markdown\/(.*)/)[1]);
-   res.write( urlValue);
-   console.log(marked(urlValue))
- }
- res.end();
+  res.writeHead(200, {"Content-Type": "text/html"});
+  if (req.url === "/") {
+    fs.readFile('index.html', 'utf8', function(err, data){
+      res.end(data);
+    })
+  } else if(req.url.match("/markdown/")) {
+    var urlValue = "";
+    urlValue = decodeURI(req.url.match(/markdown\/(.*)/)[1]);
+    res.write( urlValue);
+    console.log(marked(urlValue))
+  }
 }
